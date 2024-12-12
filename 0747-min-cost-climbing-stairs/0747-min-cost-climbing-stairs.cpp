1848608b77acf0e0985f3c5013ee84175ae21881
JAYESH SHARMA <jayeshiscoding@gmail.com>
2024-12-12 12:43:06 +0530

    Time: 0 ms (100%), Space: 17.3 MB (26.5%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -7,8 +7,14 @@ public:
         return dp[i]=cost[i]+min(helper(cost,i-1,dp),helper(cost,i-2,dp));
     }
     int minCostClimbingStairs(vector<int>& cost) {
-        int n = cost.size();
-        vector<int> dp(n, -1);
-        return min(helper(cost,n-1,dp),helper(cost,n-2,dp));
+        // int n = cost.size();
+        // vector<int> dp(n, -1);
+        // return min(helper(cost,n-1,dp),helper(cost,n-2,dp));
+
+        int n=cost.size();
+        for(int i=2;i<n;i++){
+            cost[i]+=min(cost[i-1],cost[i-2]);
+        }
+        return min(cost[n-1],cost[n-2]);
     }
 };
