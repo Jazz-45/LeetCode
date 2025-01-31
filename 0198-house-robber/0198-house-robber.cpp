ecb8250c5f3b81c4d6c11ec01fb09ab335ce80de
JAYESH SHARMA <jayeshiscoding@gmail.com>
2025-01-31 18:18:13 +0530

    Time: 0 ms (100%), Space: 10.2 MB (79.11%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,13 +2,15 @@ class Solution {
 public:
     vector<int> dp;
     int f(vector<int>& arr,int i){
-        if(i==arr.size()-1) return arr[i]; // single house
-        if(i==arr.size()-2) return max(arr[i],arr[i+1]); // 2 houses
+        int n=arr.size();
+        if(i==n-1) return arr[i]; // single house
+        if(i==n-2) return max(arr[i],arr[i+1]); // 2 houses
         return max(arr[i]+f(arr,i+2), 0+f(arr,i+1));
     }
     int ftd(vector<int>& arr,int i){
-        if(i==arr.size()-1) return arr[i]; // single house
-        if(i==arr.size()-2) return max(arr[i],arr[i+1]); // 2 houses
+        int n=arr.size();
+        if(i==n-1) return arr[i]; // single house
+        if(i==n-2) return max(arr[i],arr[i+1]); // 2 houses
         if(dp[i]!=-1) return dp[i];
         return dp[i]=max(arr[i]+ftd(arr,i+2), 0+ftd(arr,i+1));
     }
