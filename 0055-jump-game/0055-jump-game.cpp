2b39246948a9a03e107c98408903db2db5e37633
JAYESH SHARMA <jayeshiscoding@gmail.com>
2025-01-31 19:29:42 +0530

    Time: 0 ms (100%), Space: 52.3 MB (30.99%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+
+        bool flag=true;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]!=0) continue;
+
+            flag=false;
+            int count=1;
+            int j=i-1;
+            while(j>=0){
+                if(nums[j]>count){
+                    flag=true;
+                    break;
+                }
+                else{
+                    count++;
+                    j--;
+                }
+            }
+            i=j;
+        }
+        return flag;
+    }
+};
