8e58d1bcb41f96818d6c47d76cb20c8fc0d0ae91
JAYESH SHARMA <jayeshiscoding@gmail.com>
2025-03-16 18:52:28 +0530

    Time: 10 ms (71%), Space: 53.1 MB (63.11%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,46 +1,37 @@
 class Solution {
 public:
+    bool dfs(vector<vector<int>> &adj,vector<int>& vis,vector<int>& pathVis, int node,vector<int>& check){
+        vis[node]=1;
+        pathVis[node]=1;
+        for(int x: adj[node]){
+            if(!vis[x]){
+                if(dfs(adj,vis,pathVis,x,check)) return true;
+            }
+            else{
+                if(pathVis[x]) return true;
+            }
+        }
+        check[node]=1;
+        pathVis[node]=0;
+        return false;
+    }
+
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<int> indegree(n);
-        vector<vector<int>> adj(n);
+        int n=graph.size();
 
-        for (int i = 0; i < n; i++) {
-            for (auto node : graph[i]) {
-                adj[node].push_back(i);
-                indegree[i]++;
+        vector<int> vis(n,0);
+        vector<int> pathVis(n,0);
+        vector<int> safeStates;
+        vector<int> check(n,0);
+        
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                dfs(graph,vis,pathVis,i,check);
             }
         }
-
-        queue<int> q;
-        // Push all the nodes with indegree zero in the queue.
-        for (int i = 0; i < n; i++) {
-            if (indegree[i] == 0) {
-                q.push(i);
-            }
+        for(int i=0;i<n;i++){
+            if(check[i]) safeStates.push_back(i);
         }
-
-        vector<bool> safe(n);
-        while (!q.empty()) {
-            int node = q.front();
-            q.pop();
-            safe[node] = true;
-
-            for (auto& neighbor : adj[node]) {
-                // Delete the edge "node -> neighbor".
-                indegree[neighbor]--;
-                if (indegree[neighbor] == 0) {
-                    q.push(neighbor);
-                }
-            }
-        }
-
-        vector<int> safeNodes;
-        for (int i = 0; i < n; i++) {
-            if (safe[i]) {
-                safeNodes.push_back(i);
-            }
-        }
-        return safeNodes;
+        return safeStates;
     }
 };
