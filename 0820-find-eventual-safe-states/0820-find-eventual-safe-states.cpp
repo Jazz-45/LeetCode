0ff7c172f83868e3f566fe56bd5b864e254b1872
JAYESH SHARMA <jayeshiscoding@gmail.com>
2025-03-24 13:54:31 +0530

    Time: 51 ms (39.52%), Space: 66.2 MB (42.12%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,37 +1,65 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>> &adj,vector<int>& vis,vector<int>& pathVis, int node,vector<int>& check){
-        vis[node]=1;
-        pathVis[node]=1;
-        for(int x: adj[node]){
-            if(!vis[x]){
-                if(dfs(adj,vis,pathVis,x,check)) return true;
-            }
-            else{
-                if(pathVis[x]) return true;
-            }
-        }
-        check[node]=1;
-        pathVis[node]=0;
-        return false;
-    }
+    // bool dfs(vector<vector<int>> &adj,vector<int>& vis,vector<int>& pathVis, int node,vector<int>& check){
+    //     vis[node]=1;
+    //     pathVis[node]=1;
+    //     for(int x: adj[node]){
+    //         if(!vis[x]){
+    //             if(dfs(adj,vis,pathVis,x,check)) return true;
+    //         }
+    //         else{
+    //             if(pathVis[x]) return true;
+    //         }
+    //     }
+    //     check[node]=1;
+    //     pathVis[node]=0;
+    //     return false;
+    // }
 
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n=graph.size();
+        // vector<int> vis(n,0),pathVis(n,0),check(n,0);
+        // vector<int> safeStates;
+        // for(int i=0;i<n;i++){
+        //     if(!vis[i]) dfs(graph,vis,pathVis,i,check);
+        // }
+        // for(int i=0;i<n;i++){
+        //     if(check[i]) safeStates.push_back(i);
+        // }
+        // return safeStates;
 
-        vector<int> vis(n,0);
-        vector<int> pathVis(n,0);
-        vector<int> safeStates;
-        vector<int> check(n,0);
+        // BFS Kahn's Algorithm
+        
+        // Making a new reverse direction list
+        vector<vector<int>> adj(n,vector<int>());
+        for(int i=0;i<n;i++){
+            for(int x: graph[i]) adj[x].push_back(i); 
+        }
+
+        vector<int> ans;
+        queue<int> q;
+        
+        vector<int> inDegree(n,0);
+        for(int i=0;i<n;i++){
+            for(int x: adj[i]) inDegree[x]++;
+        }
         
         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                dfs(graph,vis,pathVis,i,check);
+            if(inDegree[i]==0) q.push(i);
+        }
+        
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            
+            for(int x: adj[node]){
+                inDegree[x]--;
+                if(inDegree[x]==0) q.push(x);
             }
+            ans.push_back(node);
         }
-        for(int i=0;i<n;i++){
-            if(check[i]) safeStates.push_back(i);
-        }
-        return safeStates;
+        sort(ans.begin(),ans.end());
+        return ans;
+
     }
 };
