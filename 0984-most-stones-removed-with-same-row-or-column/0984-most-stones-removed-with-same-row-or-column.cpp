10f6cfb41b1c6e43fd7b938c4b65df9100b03308
JAYESH SHARMA <jayeshiscoding@gmail.com>
2025-05-04 16:17:53 +0530

    Time: 19 ms (62.69%), Space: 25.4 MB (21.52%) - LeetHub

---
+++ 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,61 @@
+class DisjointSet{
+    public:
+        vector<int> rank,parent,size;
+        DisjointSet(int n){
+            rank.resize(n+1,0);
+            size.resize(n+1,1);
+            parent.resize(n+1);
+            for(int i=0;i<=n;i++) parent[i]=i;
+        }
+
+        int findParent(int node){
+            if(node==parent[node]) return node;
+            return parent[node]=findParent(parent[node]);
+        }
+
+        void unionBySize(int u,int v){
+            int ulp_u=findParent(u);
+            int ulp_v=findParent(v);
+
+            if(ulp_u==ulp_v) return;
+
+            if(size[ulp_u]>size[ulp_v]){
+                parent[ulp_v]=ulp_u;
+                size[ulp_u]+=size[ulp_v];
+            }
+            else{
+                parent[ulp_u]=ulp_v;
+                size[ulp_v]+=size[ulp_u];
+            }
+        }
+};
+
+class Solution {
+public:
+    int removeStones(vector<vector<int>>& stones) {
+        // Figuring out the dimensions
+        int maxRow=0,maxCol=0;
+        for(auto it: stones){
+            maxRow=max(maxRow,it[0]);
+            maxCol=max(maxCol,it[1]);
+        }
+
+        DisjointSet ds(maxRow+maxCol+1);
+
+        unordered_map<int, int> stoneNodes;
+        for (auto it : stones) {
+            int nodeRow = it[0];
+            int nodeCol = it[1] + maxRow + 1;
+            ds.unionBySize(nodeRow, nodeCol);
+            stoneNodes[nodeRow] = 1;
+            stoneNodes[nodeCol] = 1;
+        }
+
+        int cnt = 0;
+        for (auto it : stoneNodes) {
+            if (ds.findParent(it.first) == it.first) cnt++;
+        }
+        return stones.size() - cnt;
+
+    }
+};
