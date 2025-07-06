b3abf972bf6229a796deed8059942f0506426156
JAYESH SHARMA <jayeshiscoding@gmail.com>
2025-07-06 21:44:06 +0530

    Time: 19 ms (31.91%), Space: 11.3 MB (17.77%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -1,38 +1,20 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int len = s.length();
-        int left = 0, right = 0;
-        // Track frequency of a, b, c
-        vector<int> freq(3, 0);
-        int total = 0;
+        int n=s.length();
+        unordered_map<char,int> m;
+        m['a']=-1;
+        m['b']=-1;
+        m['c']=-1;
 
-        while (right < len) {
-            // Add character at right pointer to frequency array
-            char curr = s[right];
-            freq[curr - 'a']++;
-
-            // While we have all required characters
-            while (hasAllChars(freq)) {
-                // All substrings from current window to end are valid
-                // Add count of these substrings to result
-                total += len - right;
-
-                // Remove leftmost character and move left pointer
-                char leftChar = s[left];
-                freq[leftChar - 'a']--;
-                left++;
+        int count=0;
+        for(int i=0;i<n;i++){
+            m[s[i]]=i;
+            if(m['a']!=-1 && m['c']!=-1 && m['b']!=-1){
+                int lastSeen=min({m['a'],m['b'],m['c']});
+                count+=lastSeen+1;
             }
-
-            right++;
         }
-
-        return total;
-    }
-
-private:
-    bool hasAllChars(vector<int>& freq) {
-        // Check if we have at least one of each character
-        return freq[0] > 0 && freq[1] > 0 && freq[2] > 0;
+        return count;
     }
 };
