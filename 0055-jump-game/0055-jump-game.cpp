c193ab232f5ee91f6172a52078524964a9f59326
JAYESH SHARMA <jayeshiscoding@gmail.com>
2025-07-31 14:17:26 +0530

    Time: 0 ms (100%), Space: 52.3 MB (33.29%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,39 +1,24 @@
 class Solution {
 public:
-    vector<int> dp;
-    bool helper(int idx, vector<int>& nums) {
-        if (idx >= nums.size() - 1)
-            return true;
-        if (dp[idx]!=-1) return dp[idx];
-        bool ans = false;
-        if(nums[idx]>=nums.size()-1-idx) return true;
-        for (int i = 1; i <= nums[idx]; i++) {
-            ans = ans || helper(idx + i, nums);
-        }
-        return dp[idx]=ans;
-    }
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-        // Greedy Approach
-        // bool flag=true;
-        // for(int i=n-2;i>=0;i--){
-        //     if(nums[i]!=0) continue;
-        //     flag=false;
-        //     int count=1;
-        //     int j=i-1;
-        //     while(j>=0){
-        //         if(nums[j]>count){
-        //             flag=true;
-        //             break;
-        //         }else{
-        //             count++;
-        //             j--;
-        //         }
-        //     }
-        //     i=j;
-        // }
-        // return flag;
-        dp.resize(n, -1);
-        return helper(0, nums);
+        bool flag=true;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]!=0) continue;
+            flag=false;
+            int count=1;
+            int j=i-1;
+            while(j>=0){
+                if(nums[j]>count){
+                    flag=true;
+                    break;
+                }else{
+                    count++;
+                    j--;
+                }
+            }
+            i=j;
+        }
+        return flag;
     }
 };
