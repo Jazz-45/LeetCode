2940d052d960c7635c299fe3988012415c93498b
JAYESH SHARMA <jayeshiscoding@gmail.com>
2025-08-14 12:12:48 +0530

    Time: 0 ms (100%), Space: 8.6 MB (36.88%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<int> dp;
     int helper(int n){
+        // Memoization
         if(n==1) return 1;
         if(n==2) return 2;
 
@@ -12,6 +13,26 @@ public:
     int climbStairs(int n) {
         dp.clear();
         dp.resize(n+1,-1);
-        return helper(n);
+        // return helper(n);
+
+        if(n==1) return 1;  // These lines are here because the line dp[2]=2;
+        if(n==2) return 2;  // gives error when dp is of size 1 or 2;
+
+        // dp[1]=1;
+        // dp[2]=2;
+        // for(int i=3;i<=n;i++){
+        //     dp[i]=dp[i-1]+dp[i-2];
+        // }
+        // return dp[n];
+
+        int prev=2;
+        int prev2=1;
+        int curr=prev+prev2;
+        for(int i=3;i<=n;i++){
+            curr=prev2+prev;
+            prev2=prev;
+            prev=curr;
+        }
+        return curr;
     }
 };
