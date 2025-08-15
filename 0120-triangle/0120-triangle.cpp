f94c7cc6862dc6924c38462c1369fb68c5665a06
JAYESH SHARMA <jayeshiscoding@gmail.com>
2025-08-15 17:23:48 +0530

    Time: 0 ms (100%), Space: 13.5 MB (12.14%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -12,6 +12,36 @@ public:
         int m=triangle.size(),n=triangle[m-1].size();
         dp.clear();
         dp.resize(m,vector<int>(n,INT_MAX));
-        return helper(triangle,0,0);
+        // return helper(triangle,0,0);
+
+        // Tabulation
+        // Base case Fill-up
+        // for(int j=0;j<triangle[m-1].size();j++){
+        //     dp[m-1][j]=triangle[m-1][j];
+        // }
+        // for(int i=m-2;i>=0;i--){
+        //     for(int j=0;j<triangle[i].size();j++){
+        //         dp[i][j]=triangle[i][j]+min(dp[i+1][j],dp[i+1][j+1]);
+        //     }
+        // }
+        // return dp[0][0];
+
+        //Space Optimization
+        vector<int> prev(n,INT_MAX),curr(n,INT_MAX);
+        for(int j=0;j<triangle[m-1].size();j++){
+            curr[j]=triangle[m-1][j];
+        }
+        prev=curr;
+
+        for(int i=m-2;i>=0;i--){
+            for(int j=0;j<triangle[i].size();j++){
+                curr[j]=triangle[i][j]+min(prev[j],prev[j+1]);
+            }
+            prev=curr;
+        }
+        return prev[0];
+
+
+
     }
 };
