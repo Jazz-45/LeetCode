6116f1906a439aa9606b6da66b0a8491904fe843
JAYESH SHARMA <jayeshiscoding@gmail.com>
2025-08-15 15:26:30 +0530

    Time: 0 ms (100%), Space: 9.4 MB (8.69%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-    // Memoization
-    // int helper(int sr,int sc,int er,int ec,vector<vector<int>>& dp){
-    //     if(sr==er && sc==ec) return 1;
-    //     if(sr>er || sc>ec) return 0;
-    //     if(dp[sr][sc]!=-1) return dp[sr][sc];
-    //     return dp[sr][sc]= helper(sr,sc+1,er,ec,dp)+helper(sr+1,sc,er,ec,dp);
-    // }
-    int uniquePaths(int m, int n) {
-        // vector<vector<int>> dp(m,vector<int>(n,-1));
-        // return helper(0,0,m-1,n-1,dp);
+    vector<vector<int>> dp;
+    int helper(int m,int n){
+        if(m==0 && n==0) return 1;
+        if(m<0 || n<0) return 0;
 
-        // Tabulation
-        int dp[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 || j==0) dp[i][j]=1;
-                else dp[i][j]=dp[i-1][j]+dp[i][j-1];
-            }
-        }
-        return dp[m-1][n-1];
+        if(dp[m][n]!=0) return dp[m][n];
+
+        return dp[m][n]=helper(m-1,n)+helper(m,n-1);
+    }
+    int uniquePaths(int m, int n) {
+        dp.clear();
+        dp.resize(m,vector<int>(n,0));
+        return helper(m-1,n-1);
     }
 };
