874d118ec7a1276e82ca8c2fd6619c56ebada1ae
JAYESH SHARMA <jayeshiscoding@gmail.com>
2025-08-15 15:36:29 +0530

    Time: 2 ms (18.55%), Space: 9.4 MB (33.85%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -12,6 +12,27 @@ public:
     int uniquePaths(int m, int n) {
         dp.clear();
         dp.resize(m,vector<int>(n,0));
-        return helper(m-1,n-1);
+
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || j==0) dp[i][j]=1;
+                else dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        // return dp[m-1][n-1];
+        // return helper(m-1,n-1);
+
+
+        // Space optimization
+        vector<int> prev(n, 1), curr(n, 1); // first row is all 1's
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                curr[j] = curr[j-1] + prev[j];
+            }
+            prev = curr; // move to next row
+        }
+        return prev[n-1];
     }
 };
