5a7c5de8f1510e6f25832c0934b9ff55e7640f13
JAYESH SHARMA <jayeshiscoding@gmail.com>
2025-08-15 16:42:52 +0530

    Time: 0 ms (100%), Space: 16.4 MB (40.97%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -13,6 +13,33 @@ public:
         int m=grid.size(),n=grid[0].size();
         dp.clear();
         dp.resize(m,vector<int>(n,-1));
-        return helper(grid,m-1,n-1);
+        // return helper(grid,m-1,n-1);
+
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(i==0 && j==0) dp[i][j]=grid[0][0];
+        //         else{
+        //             int top=(i>0) ? dp[i-1][j] : INT_MAX;
+        //             int left=(j>0) ? dp[i][j-1] : INT_MAX;
+        //             dp[i][j]=grid[i][j]+min(top,left);
+        //         }
+        //     }
+        // }
+        // return dp[m-1][n-1];
+
+        // Space optimization
+        vector<int> curr(n,INT_MAX), prev(n,INT_MAX);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) curr[j]=grid[0][0];
+                else{
+                    int top=(i>0) ? prev[j] : INT_MAX;
+                    int left=(j>0) ? curr[j-1] : INT_MAX;
+                    curr[j]=grid[i][j]+min(top,left);
+                }
+            }
+            prev=curr;
+        }
+        return prev[n-1];
     }
 };
