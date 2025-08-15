bceafdf40ddbd99f1f40eb7ba5e6d70866b50940
JAYESH SHARMA <jayeshiscoding@gmail.com>
2025-08-15 16:06:18 +0530

    Time: 13 ms (3.67%), Space: 11.9 MB (28.61%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& arr) {
-        int m=arr.size(),n=arr[0].size();
+    vector<vector<int>> dp;
+    int helper(vector<vector<int>>& obstacleGrid,int m,int n){
+        if(m==0 && n==0) return 1;
+        if(m<0 || n<0) return 0;
 
-        // If starting or ending cell is an obstacle, return 0
-        if (arr[0][0] == 1 || arr[m-1][n-1] == 1) return 0;
+        if(obstacleGrid[m][n]==1) return dp[m][n]=0;
+        if(dp[m][n]!=0) return dp[m][n];
 
-        // Bottom - Up
-        vector<vector<int>> dp(m,vector<int>(n,0));
+        return dp[m][n]=helper(obstacleGrid,m-1,n)+helper(obstacleGrid,m,n-1);
+    }
+    
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m=obstacleGrid.size(),n=obstacleGrid[0].size();
+        if(obstacleGrid[0][0]==1) return 0;
 
-        dp[0][0]=1;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if (arr[i][j] == 1) continue;  // If obstacle, skip
-                if (i > 0) dp[i][j] += dp[i - 1][j];  // From top
-                if (j > 0) dp[i][j] += dp[i][j - 1];  // From left
-            }
-        }
-        return dp[m-1][n-1];
+        dp.clear();
+        dp.resize(m,vector<int>(n,0));
+
+        return helper(obstacleGrid,m-1,n-1);
     }
 };
