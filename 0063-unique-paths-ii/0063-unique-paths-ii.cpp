ea3c7d38556d34fb75503688af165ff66a6cee1c
JAYESH SHARMA <jayeshiscoding@gmail.com>
2025-08-15 16:22:49 +0530

    Time: 0 ms (100%), Space: 11.8 MB (54.72%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -18,6 +18,45 @@ public:
         dp.clear();
         dp.resize(m,vector<int>(n,0));
 
-        return helper(obstacleGrid,m-1,n-1);
+        // return helper(obstacleGrid,m-1,n-1);
+        for (int j=0;j<n;j++) {
+            if (obstacleGrid[0][j] == 1) break;
+            dp[0][j] = 1;
+        }
+        // initialize first col
+        for (int i=0;i<m;i++) {
+            if (obstacleGrid[i][0] == 1) break;
+            dp[i][0] = 1;
+        }
+
+        // fill the rest
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) dp[i][j] = 0;
+                else dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+
+        vector<int> dp(n, 0);
+
+        // Initialize first row
+        for (int j = 0; j < n; j++) {
+            if (obstacleGrid[0][j] == 1) break;
+            dp[j] = 1;
+        }
+
+        // Process each row
+        for (int i = 1; i < m; i++) {
+            // First column: reset to 0 if obstacle
+            if (obstacleGrid[i][0] == 1) dp[0] = 0;
+            // Other columns
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) dp[j] = 0;
+                else dp[j] += dp[j-1];
+            }
+        }
+
+        return dp[n-1];
     }
 };
