66d928287ffe45c1a9b0686fdb5485091a6d86a0
JAYESH SHARMA <jayeshiscoding@gmail.com>
2025-08-19 12:30:01 +0530

    Time: 39 ms (52.23%), Space: 49.3 MB (34.19%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>>dp;
-    int helper(vector<int>& coins, int amount,int i){
-        if(i==coins.size()){
-            if(amount==0) return 0;
-            else return INT_MAX;
+    vector<vector<int>> dp;
+    int helper(vector<int>& coins, int amount,int idx){
+        if(idx==0){
+            if(amount % coins[idx]==0) return amount/coins[idx];
+            else return 1e9; // 1e9 safer than INT_MAX
         }
-        if(dp[i][amount]!=-1) return dp[i][amount];
-        if(amount-coins[i]<0) return dp[i][amount]=helper(coins,amount,i+1);
-        int take=helper(coins,amount-coins[i],i);
-        if(take==INT_MAX) return dp[i][amount]=helper(coins,amount,i+1);
 
-        int leave=helper(coins,amount,i+1);
-        return dp[i][amount]=min(take+1,leave);
+        if(dp[idx][amount]!=-1) return dp[idx][amount];
+
+        int notTake=0+helper(coins,amount,idx-1);
+        
+        int take=1e9;
+        if(coins[idx]<=amount) take=1+helper(coins,amount-coins[idx],idx);
+
+        return dp[idx][amount]=min(take,notTake);
     }
     int coinChange(vector<int>& coins, int amount) {
-
-        
-        dp.resize(coins.size(),vector<int>(amount+1,-1));
-        int ans=helper(coins,amount,0);
-        if(ans==INT_MAX) return -1;
-        return ans;
+        int n=coins.size();
+        dp.clear();
+        dp.resize(n,vector<int>(amount+1,-1));
+        int ans=helper(coins,amount,n-1);
+        return (ans>=1e9 ? -1 : ans);
     }
 };
