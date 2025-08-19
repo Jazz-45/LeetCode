858b1ceab37fc0a839c21194d5805ccffe39b53c
JAYESH SHARMA <jayeshiscoding@gmail.com>
2025-08-19 17:11:46 +0530

    Time: 87 ms (7.48%), Space: 57.3 MB (6.82%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -20,7 +20,49 @@ public:
         int n=coins.size();
         dp.clear();
         dp.resize(n,vector<int>(amount+1,-1));
-        int ans=helper(coins,amount,n-1);
-        return (ans>=1e9 ? -1 : ans);
+        //int ans=helper(coins,amount,n-1);
+        //return (ans>=1e9 ? -1 : ans);
+
+        //Tabulation
+        // Base Case
+        for(int t=0;t<=amount;t++){
+            if(t%coins[0]==0) dp[0][t]=t/coins[0];
+            else dp[0][t]=1e9;
+        }
+
+        //Other Case
+        for(int idx=1;idx<n;idx++){
+            for(int target=0;target<=amount;target++){
+                int notTake=0+dp[idx-1][target];
+                
+                int take=1e9;
+                if(coins[idx]<=target) take=1+dp[idx][target-coins[idx]];
+
+                dp[idx][target]=min(take,notTake);
+            }
+        }
+        //return (dp[n-1][amount] >= 1e9 ? -1 : dp[n-1][amount]);
+
+
+        // Space Optimization
+        vector<int> prev(amount+1,-1),curr(amount+1,-1);
+        for(int t=0;t<=amount;t++){
+            if(t%coins[0]==0) prev[t]=t/coins[0];
+            else prev[t]=1e9;
+        }
+
+        //Other Case
+        for(int idx=1;idx<n;idx++){
+            for(int target=0;target<=amount;target++){
+                int notTake=0+prev[target];
+                
+                int take=1e9;
+                if(coins[idx]<=target) take=1+curr[target-coins[idx]];
+
+                curr[target]=min(take,notTake);
+            }
+            prev=curr;
+        }
+        return (prev[amount] >= 1e9 ? -1 : prev[amount]);
     }
 };
