cd9c660db2d6bcc95b9533d04f2cf929a1a0e53d
JAYESH SHARMA <jayeshiscoding@gmail.com>
2025-08-19 18:40:09 +0530

    Time: 42 ms (8.66%), Space: 80.9 MB (6.91%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,6 +1,8 @@
+using u128 = unsigned __int128;
+
 class Solution {
 public:
-    vector<vector<int>> dp;
+    vector<vector<u128>> dp;
     int helper(int amount, vector<int>& coins,int idx){
         if(amount==0) return 1;
         if(idx==0) return (amount%coins[idx]==0);
@@ -16,7 +18,24 @@ public:
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
         dp.clear();
-        dp.resize(n,vector<int>(amount+1,-1));
-        return helper(amount,coins,n-1);
+        dp.resize(n,vector<u128>(amount+1,0));
+        //return helper(amount,coins,n-1);
+
+        //Tabulation
+        //Base Case
+        for(int i=0;i<n;i++) dp[i][0]=1;
+        for (int t=0;t<=amount;t++) dp[0][t]=(t%coins[0]==0);
+        //Other Cases
+        for(int i=1;i<n;i++){
+            for(int t=0;t<=amount;t++){
+                u128 notTake=dp[i-1][t];
+                u128 take=0;
+                if(coins[i]<=t) take=dp[i][t-coins[i]];
+                dp[i][t]=notTake+take;
+            }
+        }
+        return (int)dp[n-1][amount];
+
+
     }
 };
