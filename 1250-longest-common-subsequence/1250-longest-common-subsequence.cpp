23ffd60be1018e32490db89ffc9df922fb52d60a
JAYESH SHARMA <jayeshiscoding@gmail.com>
2025-08-21 19:36:20 +0530

    Time: 39 ms (30.43%), Space: 27.5 MB (28.95%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -27,6 +27,19 @@ public:
                 else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
             }
         }
-        return dp[m][n];
+        //return dp[m][n];
+
+
+        //Space optimization
+        vector<int> prev(n+1,0),curr(n+1,0);
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(text1[i-1]==text2[j-1]) curr[j]=1+prev[j-1];
+                else curr[j]=max(prev[j],curr[j-1]);
+            }
+            prev=curr;
+        }
+        return prev[n];
+        
     }
 };
