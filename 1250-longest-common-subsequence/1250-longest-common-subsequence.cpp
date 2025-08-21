45ce0902edcc87e252c1e80870146d16fa838471
JAYESH SHARMA <jayeshiscoding@gmail.com>
2025-08-21 19:14:55 +0530

    Time: 49 ms (7.41%), Space: 27.6 MB (15.12%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> dp;
-    int lcs(string& text1, string& text2,int i , int j){
+    int helper(string& text1, string& text2,int i,int j){
         if(i<0 || j<0) return 0;
+
         if(dp[i][j]!=-1) return dp[i][j];
-        if(text1[i]==text2[j]) return dp[i][j]=1+lcs(text1,text2,i-1,j-1);
-        else{
-            return dp[i][j]=max(lcs(text1,text2,i-1,j),lcs(text1,text2,i,j-1));
-        }
+
+        if(text1[i]==text2[j]) return dp[i][j]=1+helper(text1,text2,i-1,j-1);
+        else return dp[i][j]=max(helper(text1,text2,i,j-1),helper(text1,text2,i-1,j));
     }
     int longestCommonSubsequence(string text1, string text2) {
-        int i=text1.size(),j=text2.size();
-        dp.resize(i,vector<int> (j,-1));
-        return lcs(text1,text2,i-1,j-1);
+        int m=text1.size(),n=text2.size();
+        dp.clear();
+        dp.resize(m,vector<int>(n,-1));
+
+        return helper(text1,text2,m-1,n-1);
     }
 };
