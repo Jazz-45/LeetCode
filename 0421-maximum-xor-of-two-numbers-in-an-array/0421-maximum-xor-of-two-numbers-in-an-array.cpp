1a6bc16d0c9612e6ae18d77f13b04528b88563cc
JAYESH SHARMA <jayeshiscoding@gmail.com>
2025-09-07 18:21:40 +0530

    Time: 602 ms (69.23%), Space: 184.1 MB (54.1%) - LeetHub

--- 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.cpp
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -1,62 +1,48 @@
-class Node{
-  public:
-    Node* left;
-    Node* right;
-    Node(){
-        this->right=this->left=NULL;
+class TrieNode{
+public:
+    TrieNode* link[2];
+    TrieNode(){
+        link[0]=NULL;
+        link[1]=NULL;
     }
 };
 
-class Solution {
+class Trie{
+private:
+    TrieNode* root;
 public:
-
-    int maxXorPair(Node* head,int value){
-        int curr_Xor=0;
-        Node* curr=head;
-        for(int i=31;i>=0;i--){
-            int bit=(value>>i) & 1;
-            if(bit==0){
-                if(curr->right!=NULL){
-                    curr=curr->right;
-                    curr_Xor+=(1<<i); // 2 ki power i
-                }
-                else curr=curr->left;
-            }else{
-                if(curr->left!=NULL){
-                    curr=curr->left;
-                    curr_Xor+=(1<<i);
-                }
-                else curr=curr->right;
-            }
-        }
-        return curr_Xor;
+    Trie(){
+        root=new TrieNode();
     }
-
-    void insert(Node* head,int value){
-        Node* curr=head;
+    void insert(int num){
+        TrieNode* curr=root;
         for(int i=31;i>=0;i--){
-            int bit=(value>>i) & 1;
-            if(bit==0){
-                if(curr->left==NULL){
-                    curr->left=new Node();
-                }
-                curr=curr->left;
-            }else{
-                if(curr->right==NULL){
-                    curr->right=new Node();
-                }
-                curr=curr->right;
-            }
+            int bit=(num>>i)&1;
+            if(curr->link[bit]==NULL) curr->link[bit]=new TrieNode();
+            curr=curr->link[bit];
         }
     }
-    int findMaximumXOR(vector<int>& nums) {
-        Node* head=new Node();
-        insert(head,nums[0]);
+    int getMaxXor(int num){
+        TrieNode* curr=root;
         int ans=0;
-        for(int i=1;i<nums.size();i++){
-            ans=max(ans,maxXorPair(head,nums[i]));
-            insert(head,nums[i]);
+        for(int i=31;i>=0;i--){
+            int bit=(num>>i)&1; 
+            if(curr->link[1-bit]!=NULL){
+                ans|=(1<<i);
+                curr=curr->link[1-bit];
+            }else curr=curr->link[bit];
         }
         return ans;
+    }
+};
+
+class Solution{
+public:
+    int findMaximumXOR(vector<int>& nums){
+        Trie trie;
+        for(int num:nums) trie.insert(num);
+        int maxi=0;
+        for(int num:nums) maxi=max(maxi,trie.getMaxXor(num));
+        return maxi;
     }
 };
